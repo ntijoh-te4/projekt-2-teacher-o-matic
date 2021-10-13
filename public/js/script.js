7a76c21116e52c8c758b18eb0d27bd3c33e0fd00
majs <myelovell@hotmail.com>
2021-10-13 13:44:32 +0200

    api fetch repo and fork

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,29 @@
-async function getRepositories (user){
-    return await fetch(`https://api.github.com/users/${user}/repos`,
-        {headers:{'Authorization': 'token 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'}})
-        .then(response => response.json()).then(data => data);
+const api_url = "https://api.github.com/"
+const user = "ntijoh-my-lovell"
+// hard coded user, needs to be changed into functions w/ get requests
+
+
+
+async function readFile(file) {
+    return await fetch(file)
+    .then(response => response.text())
+    .then(text => text)
+}
+async function getToken() {
+    let response = await readFile('../../token.key');
+    return response;
+}
+
+
+async function getRepositories(user) {
+    return await fetch(api_url + "users/" + user + '/repos', { method: 'GET', headers: {'Authorization': 'token' + await getToken()}})
+    .then(result => result.json())
+    .then(data => data);
+    console.log(data);
+}
+
+async function getForks(user, repository) {
+    return await fetch(api_url + "repos/" + user + "/" + repository + "forks", { method: 'GET', headers: {'Authorization': 'token' + await getToken() }})
+    .then(result => result.json())
+    .then(data => data);
 }
