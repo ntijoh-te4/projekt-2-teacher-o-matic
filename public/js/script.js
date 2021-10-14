345d7d0e86fa7150f5e5266b8ea646d5aeec127c
majs <myelovell@hotmail.com>
2021-10-14 14:13:37 +0200

    finished issue

--- public/js/script.js
+++ public/js/script.js
@@ -10,29 +10,35 @@ async function readFile(file) {
     .then(text => text)
 }
 async function getToken() {
-    let response = await readFile('../../api.key');
+    const response = await readFile('../api.key');
     return response;
 }
 
-function api() {
-    //adds eventlistener on input
-    document.querySelector("#search").addEventListener("input", api)
-    let user = document.querySelector("#search").value
-    
-    async function getRepositories() {
-        return await fetch(api_url + "users/" + user + '/repos', { method: 'GET', headers: {'Authorization': 'token' + await getToken()}})
-        .then(result => result.json())
-        .then(data => data);
-    }
-    
-    async function getForks() {
-        return await fetch(api_url + "repos/" + user + "/" + repository + "forks", { method: 'GET', headers: {'Authorization': 'token' + await getToken() }})
-        .then(result => result.json())
-        .then(data => data);
-    }
+//adds eventlistener on input
+document.querySelector("#user-search").addEventListener("submit", api);
 
-    getRepositories();
-    getForks();
+async function getRepositories(user) {
+    //const token = await getToken();
+    //console.log(token);
+    const response = await fetch(api_url + "users/" + user + '/repos', { method: 'GET', headers: {'Authorization': 'token ' + await getToken()  }})
+    const json = await response.json()
+    console.log(response);
+    console.log(json);
+    return json.data;
+    //returns a promise containing a map with repos abd related data
+}
+
+async function getForks(user) {
+    const response = await fetch(api_url + "repos/" + user + "/" + repository + "forks", { method: 'GET', headers: {'Authorization': 'token ' + await getToken() }})
+    const json = await response.json()
+    return json.data;
+}
+
+function api(e) {
+    e.preventDefault();
+    const user = document.querySelector("input").value   
+    getRepositories(user);
+    //getForks();
 }
 
 
