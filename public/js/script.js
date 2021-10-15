08231cbed38ddb0d88819ec085eda3235fed2609
evelinajansen <89136827+evelinajansen@users.noreply.github.com>
2021-10-15 10:50:07 +0200

    console logs all names

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,5 @@
 const api_url = "https://api.github.com/"
 // const user = "ntijoh-my-lovell"
-// hard coded user, needs to be changed into functions w/ get requests
-
-
 
 async function readFile(file) {
     return await fetch(file)
@@ -21,26 +18,13 @@ async function getRepositories(user) {
     const response = await fetch(api_url + "users/" + user + '/repos', { method: 'GET', headers: {'Authorization': 'token ' + await getToken()  }})
     const resp_body = await response.json()
     
-    console.log(resp_body[0].name);
-
+    resp_body.forEach(repo => {
+        console.log(repo.name)
+    });
+    //this is how we get the first name: console.log(resp_body[0].name);
+    //when the repository is clicked we need to call getForks(user,the_repository_that_has_been_clicked)
     return resp_body.data;
-    //returns a promise containing a map with repos abd related data
-
-
-    //const response = await fetch(api_url + "users/" + user + '/repos', { method: 'GET', headers: {'Authorization': 'token ' + await getToken()  }})
-    //.then(response => response.json()).then(data => data).then(name => name);
-    //console.log(response.get("name"));
-    //return response;
 } 
-
-
-
-
-
-
-
-
-
 
 async function getForks(user, repository) {
     const response = await fetch(api_url + "repos/" + user + "/" + repository + "forks", { method: 'GET', headers: {'Authorization': 'token ' + await getToken() }})
